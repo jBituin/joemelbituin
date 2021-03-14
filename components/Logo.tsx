225d1ae9d5f5dc791306cf819dc706f46792f244
Joemel <bituinjoemel@gmail.com>
2021-03-14 20:46:04 +0800

    Add Logo component

---
+++ components/Logo.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Box, Avatar } from '@chakra-ui/react';
+
+interface Props {
+  w: string;
+  color: string[];
+}
+
+export const Logo: React.FC<Props> = (props) => {
+  const avatarImageUrl =
+    'https://avatars.githubusercontent.com/u/29895128?s=460&u=832604e22a3e97ae92d803407757b7ad3978468d&v=4';
+  const avatarImageName = 'Joemel Bituin';
+  return (
+    <Box {...props}>
+      <Avatar name={avatarImageName} src={avatarImageUrl}>
+        JOEMEL
+      </Avatar>
+    </Box>
+  );
+};
