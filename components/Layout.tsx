2350d7b494e922e197d67ae671133877d22e189c
Joemel <bituinjoemel@gmail.com>
2021-03-24 23:45:35 +0800

    Add responsiveness to layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,11 +1,17 @@
-import { Container, Box } from '@chakra-ui/react';
+import { Flex } from '@chakra-ui/react';
 import { Navbar } from './Navbar';
 import { Footer } from './Footer';
 
-export const Layout: React.FC = ({ children }) => (
-  <Box>
+export const Layout: React.FC = ({ children, ...rest }) => (
+  <Flex
+    direction='column'
+    align='center'
+    maxW={{ xl: '1200px' }}
+    m='0 auto'
+    {...rest}
+  >
     <Navbar color='black' bg='white' />
-    <Box>{children}</Box>
+    {children}
     <Footer />
-  </Box>
+  </Flex>
 );
