eb344a636de7207157a2af6faa36bb45063f2763
Joemel <bituinjoemel@gmail.com>
2021-04-02 11:43:06 +0800

    Add bg and omdify content

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -9,15 +9,15 @@ import {
   Text,
 } from '@chakra-ui/react';
 
-interface Props {
-  title: string;
-  subtitle: string;
-  image: string;
-  ctaText: string;
-  ctaLink: string;
-}
-export const Hero: React.FC<Props> = (props: Props, { ...rest }) => {
-  const { title, subtitle, image, ctaLink, ctaText } = props;
+// const heroProps = {
+//   title: "Hey, I'm Joemel",
+//   subtitle: 'a full stack developer with 2 years of experience.',
+//   image: 'https://source.unsplash.com/AWtfHYmRCP8/800x600',
+//   ctaText: 'Know More',
+//   ctaLink: '/signup',
+// };
+
+export const Hero: React.FC = ({ ...rest }) => {
   return (
     <Flex
       align='center'
@@ -25,44 +25,57 @@ export const Hero: React.FC<Props> = (props: Props, { ...rest }) => {
       direction={{ base: 'column-reverse', md: 'row' }}
       wrap='nowrap'
       minH='70vh'
+      width='100%'
       px={8}
       mb={16}
+      backgroundImage={{ xl: 'url(../development.svg)' }}
+      backgroundRepeat='no-repeat'
+      backgroundPosition={'right 1rem bottom'}
+      backgroundSize='50%'
       {...rest}
     >
       <Stack
         spacing={4}
-        w={{ base: '80%', md: '40%' }}
+        w={{ base: '80%', md: '60%' }}
         align={['center', 'center', 'flex-start', 'flex-start']}
       >
         <Heading
           as='h1'
-          size='xl'
+          size='3xl'
           fontWeight='bold'
           color='primary.800'
-          textAlign={['center', 'center', 'left', 'left']}
+          m='2'
+          w='100%'
+          textAlign={['left', 'left', 'left', 'left']}
         >
-          {title}
+          Hey I'm{' '}
+          <Text as='span' color='tomato'>
+            Joemel
+          </Text>
         </Heading>
         <Heading
           as='h2'
-          size='md'
+          size='2xl'
           color='primary.800'
-          opacity='0.8'
+          m='2'
           fontWeight='normal'
           lineHeight={1.5}
-          textAlign={['center', 'center', 'left', 'left']}
+          textAlign='left'
+          w={{ lg: '70%' }}
         >
-          {subtitle}
+          a full stack developer with 2 years of experience.
         </Heading>
         <Button
           background='tomato'
-          borderRadius='8px'
+          borderRadius='0'
+          width={{ xs: 'full', sm: 'full', md: 'unset' }}
           py='4'
           px='4'
+          m='2'
           lineHeight='1'
           size='md'
         >
-          {ctaText}
+          Know More
         </Button>
         <Text
           fontSize='xs'
@@ -72,9 +85,6 @@ export const Hero: React.FC<Props> = (props: Props, { ...rest }) => {
           opacity='0.6'
         ></Text>
       </Stack>
-      <Box w={{ base: '80%', sm: '60%', md: '50%' }} mb={{ base: 12, md: 0 }}>
-        <Image src={image} size='100%' rounded='1rem' shadow='2xl' />
-      </Box>
     </Flex>
   );
 };
