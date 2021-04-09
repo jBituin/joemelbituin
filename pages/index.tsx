417cb1054e796fc8467e35ebd55ba061e1400f24
Joemel <bituinjoemel@gmail.com>
2021-04-09 22:23:05 +0800

    Display Connect section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
 import { Layout } from '../components/Layout';
 import { Hero } from '../components/Hero';
 import { About } from '../components/About';
+import { Connect } from '../components/Connect';
 
 export default function Home() {
   return (
     <Layout>
       <Hero />
       <About />
+      <Connect />
     </Layout>
   );
 }
