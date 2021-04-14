f03999d87fc7c36038e1edd82aee525f3d28b72f
Joemel <bituinjoemel@gmail.com>
2021-04-15 01:28:38 +0800

    Add sticky navbar

--- components/Navbar/NavbarContainer.tsx
+++ components/Navbar/NavbarContainer.tsx
@@ -2,13 +2,13 @@ import { Flex, Box } from '@chakra-ui/react';
 
 export const NavbarContainer: React.FC = ({ children, ...props }) => {
   return (
-    <Box w='100%'>
+    <Box w='100%' position='sticky' top='0'>
       <Flex
         as='nav'
         align='center'
         justify='space-between'
         wrap='wrap'
-        w='100%'
+        width='100%'
         p={8}
         bg={['primary.500', 'primary.500', 'transparent', 'transparent']}
         color={['white', 'white', 'primary.700', 'primary.700']}
