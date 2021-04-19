9f804e3cc7354b8b14efdca1a8c625679ffc06f4
Joemel <bituinjoemel@gmail.com>
2021-04-20 05:09:26 +0800

    Add app title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import { Layout } from '../components/Layout';
 import { Hero } from '../components/Hero';
 import { About } from '../components/About';
@@ -6,6 +7,10 @@ import { Connect } from '../components/Connect';
 export default function Home() {
   return (
     <Layout>
+      <Head>
+        <title>Joemel Bituin</title>
+        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+      </Head>
       <Hero />
       <About />
       <Connect />
