5028fc6cd0b7974496b7ae7fe8ff926c5359b7f7
Joemel <bituinjoemel@gmail.com>
2021-04-27 22:37:35 +0800

    Fix navbar import

--- components/index.ts
+++ components/index.ts
@@ -5,7 +5,7 @@ export * from './Layout';
 export * from './Footer';
 export * from './Hero';
 export * from './IntroLine';
-export * from './Layout';
+export * from './Navbar';
 export * from './Logo';
 export * from './MotionBox';
 export * from './MotionText';
